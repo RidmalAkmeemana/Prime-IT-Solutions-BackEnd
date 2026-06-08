68b5d51caf7d64db194ef723887acccec21d16ee
Ridmal <rajeewaakmeemana@gmail.com>
2026-06-08 20:04:29 +0530

    Production Release 2026.06.08

--- API/Connection/uploadurl.php
+++ API/Connection/uploadurl.php
@@ -1,4 +1,4 @@
 <?php
-    $base_url="https://admin.pitsl.com/";
+    $base_url="https://admin.pitsl.com/Prime-IT-Solutions-BackEnd/";
     //$base_url="http://localhost/Prime-IT-Solutions-BackEnd/";
 ?>
